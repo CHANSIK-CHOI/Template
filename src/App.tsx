b2b3ef8d70c148c0a982ed0db67ed7b5254f0cf3
CHANSIK-CHOI <ckstlr0828@frameout.co.kr>
2025-11-15 22:36:55 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <main>
+      <div className="layout-center">
+        <h1>Title</h1>
+        <p>Sub Text</p>
+      </div>
+    </main>
+  )
+}
+
+export default App
